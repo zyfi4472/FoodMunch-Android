d45b7593aaad5230eb8fa90a274991329e7335b0
zyfi4472 <zyfi4472@gmail.com>
2022-01-01 21:24:12 +0500

    login register click listener

--- app/src/main/java/com/example/foodmunch/MainActivity.java
+++ app/src/main/java/com/example/foodmunch/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import java.util.Objects;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(() -> {
--- app/src/main/java/com/example/foodmunch/register.java
+++ app/src/main/java/com/example/foodmunch/register.java
@@ -5,19 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class login extends AppCompatActivity {
+public class register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_register);
     }
 
-
-    public void signup(View view) {
-        Intent i = new Intent(login.this , register.class);
+    public void login(View view) {
+        Intent i = new Intent(register.this , login.class);
         startActivity(i);
     }
 }
