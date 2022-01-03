0c0afb96cf9d12e078bb0f2b46ded11850b19ad8
zyfi4472 <zyfi4472@gmail.com>
2022-01-04 00:23:12 +0500

    click listener on Nav-Drawer menu items

--- app/src/main/java/com/example/foodmunch/home.java
+++ app/src/main/java/com/example/foodmunch/home.java
@@ -1,13 +1,16 @@
 package com.example.foodmunch;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class home extends AppCompatActivity {
+public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -20,17 +23,40 @@ public class home extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(home.this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
         }
 
+    }
 
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId())
+        {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.nav_myaccount:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyaccountFragment()).commit();
+                break;
+            case R.id.nav_delivery:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DeliveryFragment()).commit();
+                break;
+            case R.id.nav_myorders:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyordersFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     @Override
     public void onBackPressed() {
         if(drawer.isDrawerOpen(GravityCompat.START))
